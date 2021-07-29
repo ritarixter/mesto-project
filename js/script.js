404c3408127b81f152f73971cca969907ec4a652
Rita Rixter <ritarixter@mail.ru>
2021-07-29 23:46:36 +0300

    Добавила функционал js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,85 @@
-let popup = document.querySelector('.popup');
-let popup_close = popup.querySelector('.btn_function_close');
+let popup_profile = document.querySelector('.popup__profile-edit');
+let popup_card= document.querySelector('.popup__card-edit');
+let popup_close_profile = document.querySelector('.popup__btn-profile');
+let popup_close_card = document.querySelector('.popup__btn-card');
 let edit_button = document.querySelector('.profile__edit-button');
-let popup_inputs =  popup.querySelectorAll('.popup__input');
+let add_button = document.querySelector('.profile__add-button');
+let popup_inputs =  document.querySelectorAll('.popup__input');
 let profile__name = document.querySelector('.profile__name');
 let profile__prof = document.querySelector('.profile__prof');
+const cards = document.querySelector('.elements');
+const popup__card_save = document.querySelector('.popup__card-save');
+const likes = document.querySelectorAll('.element__btn');
+const cards_delete = document.querySelectorAll('.element__delete');
+
 edit_button.addEventListener('click', function(){
-  popup.classList.add('popup_opened');
+  popup_profile.classList.add('popup_opened');
   popup_inputs[0].value = profile__name.textContent;
   popup_inputs[1].value = profile__prof.textContent;
 });
 
-popup_close.addEventListener('click', function(){
-  popup.classList.remove('popup_opened');
+add_button.addEventListener('click', function(){
+  popup_card.classList.add('popup_opened');
+  //popup_inputs[0].value = profile__name.textContent;
+  //popup_inputs[1].value = profile__prof.textContent;
+});
+
+popup_close_profile.addEventListener('click', function(){
+  popup_profile.classList.remove('popup_opened');
+})
+
+popup_close_card.addEventListener('click', function(){
+  popup_card.classList.remove('popup_opened');
 })
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
   profile__name.textContent = popup_inputs[0].value;
   profile__prof.textContent = popup_inputs[1].value;
-  popup.classList.remove('popup_opened');
+  popup_profile.classList.remove('popup_opened');
 }
 
-popup.addEventListener('submit', formSubmitHandler);
+popup_profile.addEventListener('submit', formSubmitHandler);
+
+function addCard (imgValue,nameValue) {
+  const cardTemplate = document.querySelector('#template-element').content;
+  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  cardElement.querySelector('.element__image').src=imgValue;
+  cardElement.querySelector('.element__name').textContent=nameValue;
+  cardElement.querySelector('.element__btn').addEventListener('click', function(evt){
+    evt.target.classList.toggle('element__btn_active');
+  })
+  cardElement.querySelector('.element__delete').addEventListener('click', function(evt){
+    const card_delete_btn = evt.target
+    const card_element = card_delete_btn.closest('.element') 
+    card_element.remove();
+  })
+  
+  cards.prepend(cardElement);
+}
+
+function cardSubmit(evt){
+  evt.preventDefault(); 
+  const popup__card_name = document.querySelector('.popup__card-name');
+  const popup__card_link = document.querySelector('.popup__card-link');
+  addCard(popup__card_link.value,popup__card_name.value);
+  popup__card_name.value='';
+  popup__card_link.value='';
+  popup_card.classList.remove('popup_opened');
+}
+
+popup_card.addEventListener('submit',cardSubmit);
+
+likes.forEach(function(like) {
+like.addEventListener('click', function(evt){
+  evt.target.classList.toggle('element__btn_active');
+})
+})
+
+cards_delete.forEach(function(card_delete) {
+card_delete.addEventListener('click', function(evt){
+  const card_delete_btn = evt.target
+  const card_element = card_delete_btn.closest('.element') 
+  card_element.remove();
+})
+})
